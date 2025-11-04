513b91e7956a53a144ebb98f34a05c13c136da82
Hannah <hmmclau3@asu.edu>
2025-11-03 19:14:57 -0800

    Create StaffHomePage.java

---
+++ src/main/java/application/StaffHomePage.java
@@ -0,0 +1,47 @@
+package application;
+
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+//=== EDITS START ===
+import javafx.scene.control.Button;
+import databasePart1.DatabaseHelper;
+//=== EDITS END === 
+
+/**
+ * This page displays a simple welcome message for the user.
+ */
+
+public class StaffHomePage {
+	// === EDITS START ===
+    private final DatabaseHelper databaseHelper;
+
+    public StaffHomePage(DatabaseHelper databaseHelper) {
+        this.databaseHelper = databaseHelper;
+    }
+    // === EDITS END ===
+
+    public void show(Stage primaryStage) {
+    	VBox layout = new VBox();
+	    layout.setStyle("-fx-alignment: center; -fx-padding: 20;");
+	    
+	    // Label to display Hello user
+	    Label userLabel = new Label("Hello, Staff!");
+	    userLabel.setStyle("-fx-font-size: 16px; -fx-font-weight: bold;");
+
+		 // === EDITS START ===
+        Button logoutBtn = new Button("Logout");
+        logoutBtn.setOnAction(e -> new UserLoginPage(databaseHelper).show(primaryStage));
+        // === EDITS END ===
+        
+        // edit layout for logout button
+        layout.getChildren().addAll(userLabel, logoutBtn);
+	    Scene userScene = new Scene(layout, 800, 400);
+
+	    // Set the scene to primary stage
+	    primaryStage.setScene(userScene);
+	    primaryStage.setTitle("Staff Page");
+    	
+    }
+}
