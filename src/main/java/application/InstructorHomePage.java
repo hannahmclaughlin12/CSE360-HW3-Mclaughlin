9a482a85c62b691451f2951cb206601644fa4e61
Hannah <hmmclau3@asu.edu>
2025-11-03 19:10:58 -0800

    Create InstructorHomePage.java

---
+++ src/main/java/application/InstructorHomePage.java
@@ -0,0 +1,48 @@
+package application;
+
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+//=== EDITS START ===
+import javafx.scene.control.Button;
+import databasePart1.DatabaseHelper;
+//=== EDITS END ===
+
+/**
+ * This page displays a simple welcome message for the user.
+ */
+
+public class InstructorHomePage {
+	// === EDITS START ===
+    private final DatabaseHelper databaseHelper;
+
+    public InstructorHomePage(DatabaseHelper databaseHelper) {
+        this.databaseHelper = databaseHelper;
+    }
+    // === EDITS END ===
+
+
+    public void show(Stage primaryStage) {
+    	VBox layout = new VBox();
+	    layout.setStyle("-fx-alignment: center; -fx-padding: 20;");
+	    
+	    // Label to display Hello user
+	    Label userLabel = new Label("Hello, Instructor!");
+	    userLabel.setStyle("-fx-font-size: 16px; -fx-font-weight: bold;");
+
+		// === EDITS START ===
+        Button logoutBtn = new Button("Logout");
+        logoutBtn.setOnAction(e -> new UserLoginPage(databaseHelper).show(primaryStage));
+        // === EDITS END ===
+
+	    //add logout button to layout
+		layout.getChildren().addAll(userLabel, logoutBtn);
+	    Scene userScene = new Scene(layout, 800, 400);
+
+	    // Set the scene to primary stage
+	    primaryStage.setScene(userScene);
+	    primaryStage.setTitle("Instructor Page");
+    	
+    }
+}
